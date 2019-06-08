UnicordDev/Unicord

commit c622995b5099e56b643cd87105f3d4c2187c3a2b

Remove and sort usings, and fix formatting across the whole project

Libraries/DSharpPlus/DSharpPlus/Entities/DiscordMember.cs
@@ -1,6 +1,3 @@
-using DSharpPlus.Net.Abstractions;
-using DSharpPlus.Net.Models;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,6 +5,9 @@ using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DSharpPlus.Net.Abstractions;
+using DSharpPlus.Net.Models;
+using Newtonsoft.Json;
 
 #if WINDOWS_UWP
 using Windows.UI.Xaml.Media;

Unicord.Universal/Controls/AttachmentViewer.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using DSharpPlus.Entities;
 using Unicord.Universal.Utilities;
 using WamWooWam.Core;
@@ -11,12 +10,10 @@ using Windows.Foundation;
 using Windows.Media.Core;
 using Windows.Storage;
 using Windows.Storage.Pickers;
-using Windows.Storage.Streams;
 using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.Web.Http;
 

Unicord.Universal/Dialogs/ProfileOverlay.xaml.cs
@@ -1,24 +1,11 @@
-﻿using DSharpPlus.Entities;
-using DSharpPlus.EventArgs;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using DSharpPlus.Entities;
 using Unicord.Universal.Commands;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI;
-using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Shapes;
 
 namespace Unicord.Universal.Dialogs

Unicord.Universal/Pages/DiscordPage.xaml.cs
@@ -7,25 +7,16 @@ using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
-using DSharpPlus.VoiceNext;
-using Microsoft.Toolkit.Uwp.UI;
 using Microsoft.Toolkit.Uwp.UI.Controls;
 using Unicord.Universal.Controls;
-using Unicord.Universal.Integration;
 using Unicord.Universal.Models;
 using Unicord.Universal.Pages.Settings;
 using Unicord.Universal.Pages.Subpages;
 using Unicord.Universal.Utilities;
-using Unicord.Universal.Voice;
-using WamWooWam.Core;
-using Windows.ApplicationModel.Contacts;
 using Windows.Foundation;
 using Windows.Foundation.Metadata;
-using Windows.System;
-using Windows.System.Profile;
 using Windows.UI;
 using Windows.UI.Core;
-using Windows.UI.Input;
 using Windows.UI.Notifications;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;

commit 1a768cc90cec23604412dcfaed7f7dd8ee145157

Fix build errors caused by https://github.com/WamWooWam/Unicord/commit/c622995b5099e56b643cd87105f3d4c2187c3a2b

Libraries/DSharpPlus/DSharpPlus/Entities/DiscordMember.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Threading;
+using Newtonsoft.Json;
+using DSharpPlus.Net.Models;
+using DSharpPlus.Net.Abstractions;
 
 #if WINDOWS_UWP
 using Windows.UI.Xaml.Media;

Unicord.Universal/Controls/AttachmentViewer.xaml.cs
@@ -14,6 +14,7 @@ using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.Web.Http;
 

Unicord.Universal/Dialogs/ProfileOverlay.xaml.cs
@@ -10,6 +10,7 @@ using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Shapes;
 

Unicord.Universal/Pages/DiscordPage.xaml.cs
@@ -9,6 +9,7 @@ using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using Microsoft.Toolkit.Uwp.UI.Controls;
 using Unicord.Universal.Controls;
+using Unicord.Universal.Integration;
 using Unicord.Universal.Models;
 using Unicord.Universal.Pages.Settings;
 using Unicord.Universal.Pages.Subpages;