ff49b51f4c2086e73f10acddcebca0cfa99dd0a0
Thomas May <wamwoowam@gmail.com>
2024-09-01 19:24:36 +0100

    The Great Line-endening™️

---
+++ Unicord.Universal/Misc/UnicordVersionProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+
+namespace Unicord.Universal.Misc
+{
+    class UnicordVersionProvider : IVersionProvider
+    {
+        public string GetVersionString()
+        {
+            var version = 70 + ((DateTime.Now.Year - 2020) * 12) + DateTime.Now.Month;
+            return $"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{version}.0.4280.141 Safari/537.36";
+        }
+    }
+}
