5acc1a4cbefc41f16d51ba664e6ef319cf9111eb
GID <elminuryati12345@gmail.com>
2025-12-26 02:38:27 +0700

    removed DisplayVersion "v" prefix

--- Unicord.Universal/Models/Settings/RootSettingsModel.cs
+++ Unicord.Universal/Models/Settings/RootSettingsModel.cs
@@ -8,6 +8,8 @@ namespace Unicord.Universal.Models
 {
     public class RootSettingsModel : ViewModelBase
     {
+        private static readonly bool _isWindows11 = SystemInformation.Instance.OperatingSystemVersion.Build >= 22000;
+
         public DiscordUser CurrentUser =>
             discord.CurrentUser;
 
@@ -30,8 +32,11 @@ namespace Unicord.Universal.Models
                     gitSha = "-" + attribute.InformationalVersion.Substring(idx + 1, 7);
                 }
 
-                return $"v{Package.Current.Id.Version.ToFormattedString(3)}{gitSha}";
+                return $"{Package.Current.Id.Version.ToFormattedString(3)}{gitSha}";
             }
         }
+
+        public string NotificationIconGlyph
+            => _isWindows11 ? "\uEA8F" : "\uE91C";
     }
 }
